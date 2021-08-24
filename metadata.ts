035140dc42bd88b81c324d844be28871806cb4b2
Adrien Martin <adrien.v.martin@gmail.com>
2021-08-23 21:40:15 -0600

    Call new method

--- metadata.ts
+++ metadata.ts
@@ -12,3 +12,7 @@ class Plane {
 function markFunction(target: Plane, key: string) {
   Reflect.defineMetadata('secret', 123, target, key);
 }
+
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+
+console.log(secret);
